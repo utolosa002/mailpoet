428c76fcefba5ec8c141e8b3591dad099a72b257
Jan Jakeš <jan@jakes.pro>
2019-05-09 15:00:51 +0200

    Add Doctrine metadata storage configuration
    [MAILPOET-2014]

--- lib/Doctrine/ConfigurationFactory.php
+++ lib/Doctrine/ConfigurationFactory.php
@@ -2,11 +2,14 @@
 
 namespace MailPoet\Doctrine;
 
+use MailPoetVendor\Doctrine\Common\Annotations\SimpleAnnotationReader;
+use MailPoetVendor\Doctrine\Common\Cache\FilesystemCache;
 use MailPoetVendor\Doctrine\ORM\Configuration;
 use MailPoetVendor\Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
 
 class ConfigurationFactory {
   const ENTITY_DIR = __DIR__ . '/../Doctrine/Entities';
+  const METADATA_DIR = __DIR__ . '/../../generated/doctrine-metadata';
 
   /** @var bool */
   private $is_dev_mode;
@@ -18,11 +21,24 @@ class ConfigurationFactory {
   function createConfiguration() {
     $configuration = new Configuration();
     $configuration->setNamingStrategy(new UnderscoreNamingStrategy());
-    $configuration->setMetadataDriverImpl($configuration->newDefaultAnnotationDriver([self::ENTITY_DIR]));
+
+    $this->configureMetadata($configuration);
 
     if (!$this->is_dev_mode) {
       $configuration->ensureProductionSettings();
     }
     return $configuration;
+  }
+
+  private function configureMetadata(Configuration $configuration) {
+    // on production load metadata only from pre-generated cache
+    $metadata_storage = new FilesystemCache(self::METADATA_DIR);
+    $metadata_storage->setNamespace('mp3-' . md5(self::METADATA_DIR) . '-');
+    $configuration->setMetadataCacheImpl($metadata_storage);
+
+    // in dev mode register annotation reader if doctrine/annotations package is installed
+    if ($this->is_dev_mode && class_exists(SimpleAnnotationReader::class)) {
+      $configuration->setMetadataDriverImpl($configuration->newDefaultAnnotationDriver([self::ENTITY_DIR]));
+    }
   }
 }
